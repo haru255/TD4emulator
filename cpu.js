391ee23e5fac4c782a92d1c8b04126bba4d2443a
haru <>
2020-10-14 13:24:16 +0900

    Register、IO、ROMの値の表示

--- cpu.js
+++ cpu.js
@@ -18,6 +18,7 @@ class Register {
     set_pc(val) {
         this.#pc = val & 0xF;
     }
+    inc_pc() {this.set_pc(this.get_pc()+1)}
     get_a() {return this.#a;}
     set_a(val) {
         this.#a = val & 0xF;
@@ -33,17 +34,23 @@ class Register {
 }
 
 class IO {
+    #input = 0;
+    #output = 0;
     constructor() {
-        this.input = 0;
-        this.output = 0;
+        this.#input = 0;
+        this.#output = 0;
     }
-    get_input() {return this.input};
-    set_output(val) {this.output = val};
+    get_input() {return this.#input;}
+    get_output() {return this.#output;}
+    set_output(val) {this.#output = val;}
 }
 
 class Rom {
-    constructor() {
-        this.rom = new Uint8Array(16);
+    constructor(el) {
+        this.rom = el.children;
+    }
+    get_rom(ind) {
+        return parseInt(this.rom[ind].value, 2);
     }
 }
 
@@ -61,8 +68,8 @@ function reset() {
 }
 //pcが指す番地のデータを受け取る
 function fetch() {
-    let res = rom.rom[register.pc];
-    register.pc ++;
+    let res = rom.get_rom(register.get_pc());
+    register.inc_pc();
     return res;
 }
 function step() {
@@ -70,13 +77,26 @@ function step() {
     let op = data >> 4;
     let im = data & 0x0F;
     opcode[op](im);
+    rendering();
 }
 
 const register = new Register();
 const io = new IO();
-const rom = new Rom();
+const rom = new Rom(document.getElementById("rom"));
 
-step();
+const el_reg_a = document.getElementById("reg-a");
+const el_reg_b = document.getElementById("reg-b");
+const el_reg_pc = document.getElementById("reg-pc");
+const el_reg_c = document.getElementById("reg-c");
+const el_output = document.getElementById("output");
+function rendering() {
+    el_reg_a.innerText = register.get_a().toString(2).padStart(4,"0");
+    el_reg_b.innerText = register.get_b().toString(2).padStart(4,"0");
+    el_reg_pc.innerText = register.get_pc().toString(2).padStart(4,"0");
+    el_reg_c.innerText = register.get_c();
+    el_output.innerText = io.get_output().toString(2).padStart(4,"0");
+}
+
 
 // 以下命令セット
 function mov_a(im) {
